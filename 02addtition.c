9a711e4605c412b42f0a5b71361ca3b9a2c7a0d3
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-08-18 21:45:23 +0530

    Add files via upload

---
+++ 02addtition.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter the no. a \n");
+    scanf("%d",&a);
+    printf("enter the no.b \n");
+    scanf("%d",&b);
+    int c=a+b;
+    int d=a-b;
+    int e=a*b;
+    printf("\n the sum of %d",c);
+    printf("\n the substarct of %d",d);
+    printf("\n the multiply of %d",e);
+    return 0;
+}
