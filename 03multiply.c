663453ed1f04e97a93e8190775996dfdbbfacf7e
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-08-19 21:24:05 +0530

    Create 03multiply.c

---
+++ 03multiply.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    int b;
+    int c;
+    printf("enter the value of a and b");
+    scanf("%d %d",&a,&b);
+    c=a*b;
+    printf("multiply of=%d",c);
+}
