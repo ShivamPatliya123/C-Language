92132c8f84ca64ff987f834cdddeeaced9dc9ba3
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-08-20 19:58:23 +0530

    05Combination.c

---
+++ 05Combination.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    a=36;
+    b=4;
+    printf("a+b=%d \n",a+b);
+    printf("a-b=%d \n",a-b);
+    printf("a*b=%d \n",a*b);
+    printf("a/b=%d \n",a/b);
+    return 0;
+}
