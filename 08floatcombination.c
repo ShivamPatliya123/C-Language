2f9d60b6b546b03861528c4e1fb3a4e94712a5c4
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-08-21 22:10:53 +0530

    Create 08floatcombination.c

---
+++ 08floatcombination.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    float a;
+    float b;
+    float c;
+    printf("enter the no a");
+    scanf("%f",&a);
+    printf("enter the no b");
+    scanf("%f",&b);
+    c=a+b;
+   float d=a-b;
+   float e=a*b;
+   float f=a/b;
+
+   printf("addition of %f ",c);
+   printf("subtraction of %f",d);
+   printf("multiply of %f",e);
+   printf("divided of %f",f);
+   
+        return 0;
+}
