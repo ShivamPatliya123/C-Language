fd9e076a120350adfae87049776f26abfd2d2634
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-08-22 21:08:20 +0530

    12simpleint.c

---
+++ 12simpleint.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    float p,r,t,S;
+    printf("enter the no p");
+    scanf("\n %f",&p);
+    printf("enter the no r");
+    scanf("\n %f",&r);
+    printf("enter the no t");
+    scanf("\n %f",&t);
+    S=(p*r*t)/100;
+    printf("\n simple intrestof=%f",S);
+    return 0;
+}
