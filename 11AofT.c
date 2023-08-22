eee83f1a81403b7befd89eb2b557c465cc33cbb7
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-08-22 20:57:27 +0530

    11AofT.c

---
+++ 11AofT.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int b,h,a;
+    printf("enter the value of b");
+    scanf("\n %d",&b);
+    printf("enter the value of h");
+    scanf("\n %d",&h);
+    a=(b*h)/2;
+    printf("\n area of triangle = %d",a);
+    return 0;
+
+}
