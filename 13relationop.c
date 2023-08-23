00b50b5428a3cf0ff46cd42443fb205012893c71
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-08-23 22:17:38 +0530

    13relationop.c

---
+++ 13relationop.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int a=45;
+    int b=64;
+    int c=45;
+    printf("\n a>b=%d",(a>b));
+    printf("\n a<b=%d",(a<b));
+    printf("\n a==b=%d",(a==b));
+    printf("\n a>=b=%d",(a>=b));
+    printf("\n a<=b=%d",(a<=b));
+    printf("\n a<=b=%d",(a<=b));
+    printf("\n a!=b=%d",(a!=b));
+    printf("\n a>c=%d",(a>c));
+    printf("\n a<c=%d",(a<c));
+    printf("\n a==c=%d",(a==c));
+    printf("\n a>=c=%d",(a>=c));
+    printf("\n a<=c=%d",(a<=c));
+    printf("\n a<=c=%d",(a<=c));
+    printf("\n a!=c=%d",(a!=c));
+    printf("\n c>b=%d",(c>b));
+    printf("\n c<b=%d",(c<b));
+    printf("\n c==b=%d",(c==b));
+    printf("\n c>=b=%d",(c>=b));
+    printf("\n c<=b=%d",(c<=b));
+    printf("\n c<=b=%d",(c<=b));
+    printf("\n c!=b=%d",(c!=b));
+    return 0;
+}
