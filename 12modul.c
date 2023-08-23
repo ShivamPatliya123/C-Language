45190c6840d7b53ed35708465ea805b7f2dc7f05
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-08-23 21:51:56 +0530

    12modul.c

---
+++ 12modul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b;
+    printf("enter the value of a");
+    scanf("\n %d",&a);
+    printf("enter the value of b");
+    scanf("\n %d",&b);
+    int c=a%b;
+    printf("modul of=%d",c);
+    return 0;
+}
