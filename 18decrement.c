7b95947b85a8afaa9ca833c5d481305c2218dc3b
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-08-24 21:56:32 +0530

    18decrement.c

---
+++ 18decrement.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a=20;
+    float b=43.56;
+    char c='b';
+    printf("\ndecrement=%d",a--);
+    printf("\na=%d",a);
+      printf("\ndedcrement=%f",b--);
+     printf("\nb=%f",b);
+       printf("\ndecrement=%d",c--);
+     printf("\na=%d",c);
+       printf("\ndecrement=%c",c--);
+     printf("\nc=%c",c);
+}
