c03b0a8f5b8b9bed9f5f1db266d9f3e2916106f6
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-08-24 21:50:34 +0530

    17increment.c

---
+++ 17increment.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    float b=21.34;
+    char c='a';
+    printf("\nincrease=%d",a++);
+    printf("\na=%d",a);
+      printf("\nincrease=%f",b++);
+     printf("\na=%f",b);
+       printf("\nincrease=%d",c++);
+     printf("\na=%d",c);
+       printf("\nincrease=%c",c++);
+     printf("\na=%c",c);
+}
