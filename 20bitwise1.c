2267b396b4a9c122cd2f74df3eb6db3f2b76a6a9
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-08-25 21:53:50 +0530

    20bitwise1.c

---
+++ 20bitwise1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char x='a';
+    char y='b';
+    printf("\n x&y=%d",(x&y)); 
+    printf("\n x|y=%d",(x|y));
+    printf("\nx^y=%d",(x^y));
+    printf("\n ~x=%d",(~x));
+    printf("\n ~y=%d",(~y));
+        return 0;
+}
