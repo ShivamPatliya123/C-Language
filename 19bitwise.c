805cdfdefec8510074a0ae5bccda0a68881d51b5
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-08-25 20:49:54 +0530

    19bitwise.c

---
+++ 19bitwise.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a=12;
+    int b=23;
+    printf("\n a&b=%d",(a&b));
+    printf("\n a|b=%d",(a|b));
+    printf("\n a^b=%d",(a^b));
+    printf("\n ~a=%d",(~a)); 
+    printf("\n ~b=%d", (~b));
+    return 0;
+}
