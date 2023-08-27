edd9f362f560c8e282c67be09e37904d04a06685
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-08-27 21:31:54 +0530

    24if.c

---
+++ 24if.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter the value of a");
+    scanf("\n %d",&a);
+      printf("enter the value of b");
+    scanf("\n %d",&b);
+    if(a>b)
+    printf("a is the max=%d");
+    else
+    printf("b is the max and a is small=%d");
+    return 0;
+}
