bbbfb3bd36bae0ba8a9e10f4eed2fad0b69c925f
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-08-27 22:30:45 +0530

    26if2.c

---
+++ 26if2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char x='d';
+    char y='p';
+    
+     if(x>y)
+     {
+        printf("\n x is greatest");
+     }
+     else
+     {
+        printf("\n y is greatest");
+     }
+     return 0;
+}
