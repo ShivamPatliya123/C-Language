fd087c6f85c7589a9d3d5e2116a7aa1110a390c6
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-09-02 20:26:51 +0530

    31conditionalop.c

---
+++ 31conditionalop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("\n Enter the value a");
+    scanf("%d",&a);
+    printf("\n Enter the value b");
+    scanf("%d",&b);
+    (a>b)?
+     printf("\n a is maximum"):printf("\n b is maximum");
+    return 0;
+}
