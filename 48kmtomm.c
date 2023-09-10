566557a10d8fb17ce345f8fd59dd0cf4a9e4849a
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-09-10 18:42:52 +0530

    48kmtomm.c

---
+++ 48kmtomm.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float km,mm;
+    printf("Enter the value ");
+    scanf("\n%f",&km);
+    mm=(km*1000000);
+    printf("milimeter=%.2f",mm);
+    return 0;
+}
