ed5bd17ed6b78492713e133987a97d714a53d5eb
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-09-19 16:00:16 +0530

    60array2d.c

---
+++ 60array2d.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int a[3][3],i,j;
+printf("Enter the value of array");
+for(i=0;i<3;i++)
+{
+    for(j=0;j<3;j++)
+    {
+        printf("\n Element a[%d][%d]:",i,j);
+        scanf("%d",&a[i][j]);
+    }
+}
+for(i=0;i<3;i++)
+{
+    for(j=0;j<3;j++)
+    {
+        printf("%3d",a[i][j]);
+    }
+    printf("\n");
+}
+return 0;
+}
