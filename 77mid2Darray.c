9fb22cd39a33dd44e174774190278562ef8786c5
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-09-27 21:07:24 +0530

    77mid2Darray.c

---
+++ 77mid2Darray.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a[6],i,mid;
+    int *p=&a[0],*p1=&i,*p2=&mid;
+    for(*p1=0;*p1<6;(*p1)++)
+    {
+      printf("Element of array a[%d]:",*p1);
+        scanf("%d",(p+i));
+    }
+    *p2=5/2;
+    printf("middle value is=%d",a[*p2]);
+    return 0;
+}
