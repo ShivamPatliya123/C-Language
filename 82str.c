877ea66d9320a59d238ba96e9f5f2b0a92a8daa1
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-09-29 19:56:43 +0530

    82str.c

---
+++ 82str.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char name[10]={'a','j','a','y'};
+    int n;
+    printf("\n input is :%s",name);
+    n=strlen(name);
+    printf("\nsize of array:%d",n);
+     
+    return 0;
+}
