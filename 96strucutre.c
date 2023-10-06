39d42856003e198c86e56a3091d077d4cb12e74f
ShivamPatliya123 <142159496+ShivamPatliya123@users.noreply.github.com>
2023-10-06 22:18:07 +0530

    96strucutre.c

---
+++ 96strucutre.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    struct student
+    {
+        int Rollno;
+        float marks;
+        char name[20];
+    }s1;
+    printf("\n Enter the roll no:");
+    scanf("%d",&s1.Rollno);
+    printf("\n Enter the marks :");
+    scanf("%f",&s1.marks);
+    printf("\n Enter the name:");
+    scanf("%s",&s1.name);
+     
+     printf("\n roll no=%d",s1.Rollno);
+     printf("\n marks=%0.2f",s1.marks);
+     printf("\n name+%s",s1.name);
+}
